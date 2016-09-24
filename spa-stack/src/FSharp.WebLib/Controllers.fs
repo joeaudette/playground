a19ae56d2ca1a1b833ee660b99ff8b33c43d9713
Ruben Bartelink <ruben@bartelink.com>
2016-09-24 18:42:12 +0100

    Tweak coercion from Async to Task

--- spa-stack/src/FSharp.WebLib/Controllers.fs
+++ spa-stack/src/FSharp.WebLib/Controllers.fs
@@ -12,92 +12,86 @@ open CSharp.Models
 
 type PatchedToDoModel = { Original:ToDoItem; Patched:ToDoItem }
 
+module ActionResult =
+    let ofAsync (res: Async<IActionResult>) =
+        res |> Async.StartAsTask
+
 [<Route("api/[controller]")>]
 type FSToDoController(c, q) =
     inherit Controller()
-    member __.Commands:IToDoCommands = c
-    member __.Queries:IToDoQueries = q
-
-
+    
+    member this.Commands:IToDoCommands = c
+    member this.Queries:IToDoQueries = q
+  
     [<HttpGet>]
-    member __.Get() =
-        async {
-               let! data = __.Queries.GetAll() |> Async.AwaitTask 
-               return new JsonResult(data) :> IActionResult }
-            |> Async.StartAsTask
-
+    member this.Get() = 
+        ActionResult.ofAsync <| async {
+            let! data = this.Queries.GetAll() |> Async.AwaitTask 
+            return JsonResult(data) :> _ } 
 
     [<HttpGet("{id}", Name = "GetFsToDo")>]
-    member __.Get(id) = 
-       async {
-            let! dataOrDefault = __.Queries.Find(id) |> Async.AwaitTask
+    member this.Get(id) =
+        ActionResult.ofAsync <| async {
+            let! dataOrDefault = this.Queries.Find(id) |> Async.AwaitTask
             match Option.ofObj dataOrDefault with 
-            | None -> return __.NotFound() :> IActionResult
-            | Some data -> return ObjectResult(data) :> IActionResult
-       } |> Async.StartAsTask
+            | None -> return this.NotFound() :> _
+            | Some data -> return ObjectResult(data) :> _ } 
 
     // create
     [<HttpPost>]
-    member __.Post([<FromBody>] item:ToDoItem) = 
-        async {
+    member this.Post([<FromBody>] item:ToDoItem) =
+        ActionResult.ofAsync <| async {
             if isNull item 
-                then return __.BadRequest() :> IActionResult
+                then return this.BadRequest() :> _
                 else
                     item.Id <- Guid.NewGuid().ToString()
-                    do! __.Commands.Add(item) |> Async.AwaitTask
+                    do! this.Commands.Add(item) |> Async.AwaitTask
                     let rv = new RouteValueDictionary()
                     rv.Add("id",item.Id)
-                    return __.CreatedAtRoute("GetFsToDo", rv, item) :> IActionResult  } 
-            |> Async.StartAsTask
+                    return this.CreatedAtRoute("GetFsToDo", rv, item) :> _  } 
 
     //update
     [<HttpPut("{id}")>]
-    member __.Put(id:String, [<FromBody>] item:ToDoItem) = 
-        async {
+    member this.Put(id:String, [<FromBody>] item:ToDoItem) =
+        ActionResult.ofAsync <| async {
             if isNull item || String.IsNullOrEmpty item.Id
-                then return __.BadRequest() :> IActionResult
+                then return this.BadRequest() :> _
                 else
-                    let! dataOrDefault = __.Queries.Find(id) |> Async.AwaitTask
+                    let! dataOrDefault = this.Queries.Find(id) |> Async.AwaitTask
                     match Option.ofObj dataOrDefault with
-                    | None -> return __.NotFound() :> IActionResult
+                    | None -> return this.NotFound() :> _
                     | Some toDo ->
-                        do! __.Commands.Update(item) |> Async.AwaitTask 
-                        return new NoContentResult() :> IActionResult } 
-            |> Async.StartAsTask
-             
+                        do! this.Commands.Update(item) |> Async.AwaitTask 
+                        return new NoContentResult() :> _ } 
 
     // http://restful-api-design.readthedocs.io/en/latest/methods.html#patch-vs-put
     // http://benfoster.io/blog/aspnet-core-json-patch-partial-api-updates
     // this method is not currently used from the client
     [<HttpPatch("{id}")>]
-    member __.Patch(id:String, [<FromBody>] patch:JsonPatchDocument<ToDoItem>) = 
-        async {
+    member this.Patch(id:String, [<FromBody>] patch:JsonPatchDocument<ToDoItem>) =
+        ActionResult.ofAsync <| async {
             if isNull patch || String.IsNullOrEmpty id
-                then return __.BadRequest() :> IActionResult
+                then return this.BadRequest() :> _
                 else
-                    let! dataOrDefault = __.Queries.Find(id) |> Async.AwaitTask
+                    let! dataOrDefault = this.Queries.Find(id) |> Async.AwaitTask
                     match Option.ofObj dataOrDefault with
-                    | None -> return __.NotFound() :> IActionResult
+                    | None -> return this.NotFound() :> _
                     | Some toDo ->
                         let patched = toDo.Copy()
-                        patch.ApplyTo(patched, __.ModelState);
-                        if __.ModelState.IsValid then
-                            do! __.Commands.Update(patched) |> Async.AwaitTask
+                        patch.ApplyTo(patched, this.ModelState);
+                        if this.ModelState.IsValid then
+                            do! this.Commands.Update(patched) |> Async.AwaitTask
                             let model = { Original = toDo; Patched = patched; }
-                            return __.Ok(model) :> IActionResult 
+                            return this.Ok(model) :> _ 
                         else
-                            return new BadRequestObjectResult(__.ModelState) :> IActionResult } 
-            |> Async.StartAsTask
-
+                            return new BadRequestObjectResult(this.ModelState) :> _ } 
 
     [<HttpDelete("{id}")>]
-    member __.Delete(id:string) = 
-        async {
-            let! dataOrDefault =  __.Queries.Find(id) |> Async.AwaitTask
+    member this.Delete(id:string) =
+        ActionResult.ofAsync <| async {
+            let! dataOrDefault =  this.Queries.Find(id) |> Async.AwaitTask
             match Option.ofObj dataOrDefault with
-            | None -> return __.NotFound() :> IActionResult
+            | None -> return this.NotFound() :> _
             | Some toDo ->
-                    do! __.Commands.Remove(toDo) |> Async.AwaitTask 
-                    return new NoContentResult() :> IActionResult } 
-            |> Async.StartAsTask
-
+                    do! this.Commands.Remove(toDo) |> Async.AwaitTask 
+                    return new NoContentResult() :> _ } 
