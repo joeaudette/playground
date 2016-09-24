ace06fab35757b4b0dc4f3827f858c8d95f8b5dc
Ruben Bartelink <ruben@bartelink.com>
2016-09-24 21:31:58 +0100

    Use implict backing fields for type arguments

--- spa-stack/src/FSharp.WebLib/Controllers.fs
+++ spa-stack/src/FSharp.WebLib/Controllers.fs
@@ -10,29 +10,26 @@ open Microsoft.AspNetCore.Routing
 open Microsoft.AspNetCore.JsonPatch
 open CSharp.Models
 
-type PatchedToDoModel = { Original:ToDoItem; Patched:ToDoItem }
+type PatchedToDoModel = { Original: ToDoItem; Patched: ToDoItem }
 
 module ActionResult =
     let ofAsync (res: Async<IActionResult>) =
         res |> Async.StartAsTask
 
 [<Route("api/[controller]")>]
-type FSToDoController(c, q) =
+type FSToDoController(commands: IToDoCommands, queries: IToDoQueries) =
     inherit Controller()
-    
-    member this.Commands:IToDoCommands = c
-    member this.Queries:IToDoQueries = q
   
     [<HttpGet>]
     member this.Get() = 
         ActionResult.ofAsync <| async {
-            let! data = this.Queries.GetAll() |> Async.AwaitTask 
+            let! data = queries.GetAll() |> Async.AwaitTask 
             return JsonResult(data) :> _ } 
 
     [<HttpGet("{id}", Name = "GetFsToDo")>]
     member this.Get(id) =
         ActionResult.ofAsync <| async {
-            let! dataOrDefault = this.Queries.Find(id) |> Async.AwaitTask
+            let! dataOrDefault = queries.Find(id) |> Async.AwaitTask
             match Option.ofObj dataOrDefault with 
             | None -> return this.NotFound() :> _
             | Some data -> return ObjectResult(data) :> _ } 
@@ -44,7 +41,7 @@ type FSToDoController(c, q) =
             if isNull item then return this.BadRequest() :> _
             else
                 item.Id <- Guid.NewGuid() |> string
-                do! this.Commands.Add(item) |> Async.AwaitTask
+                do! commands.Add(item) |> Async.AwaitTask
                 let rv = RouteValueDictionary()
                 rv.Add("id",item.Id)
                 return this.CreatedAtRoute("GetFsToDo", rv, item) :> _ } 
@@ -56,11 +53,11 @@ type FSToDoController(c, q) =
             if isNull item || String.IsNullOrEmpty item.Id then
                 return this.BadRequest() :> _
             else
-                let! dataOrDefault = this.Queries.Find(id) |> Async.AwaitTask
+                let! dataOrDefault = queries.Find(id) |> Async.AwaitTask
                 match Option.ofObj dataOrDefault with
                 | None -> return this.NotFound() :> _
                 | Some toDo ->
-                    do! this.Commands.Update(item) |> Async.AwaitTask 
+                    do! commands.Update(item) |> Async.AwaitTask 
                     return NoContentResult() :> _ } 
 
     // http://restful-api-design.readthedocs.io/en/latest/methods.html#patch-vs-put
@@ -74,22 +71,22 @@ type FSToDoController(c, q) =
             elif this.ModelState.IsValid then
                 return BadRequestObjectResult(this.ModelState) :> _
             else
-                let! dataOrDefault = this.Queries.Find(id) |> Async.AwaitTask
+                let! dataOrDefault = queries.Find(id) |> Async.AwaitTask
                 match Option.ofObj dataOrDefault with
                 | None -> return this.NotFound() :> _
                 | Some toDo ->
                     let patched = toDo.Copy()
                     patch.ApplyTo(patched, this.ModelState)
-                    do! this.Commands.Update(patched) |> Async.AwaitTask
+                    do! commands.Update(patched) |> Async.AwaitTask
                     let model = { Original = toDo; Patched = patched }
                     return this.Ok(model) :> _ }
  
     [<HttpDelete("{id}")>]
     member this.Delete(id:string) =
         ActionResult.ofAsync <| async {
-            let! dataOrDefault =  this.Queries.Find(id) |> Async.AwaitTask
+            let! dataOrDefault =  queries.Find(id) |> Async.AwaitTask
             match Option.ofObj dataOrDefault with
             | None -> return this.NotFound() :> _
             | Some toDo ->
-                do! this.Commands.Remove(toDo) |> Async.AwaitTask 
+                do! commands.Remove(toDo) |> Async.AwaitTask 
                 return NoContentResult() :> _ }
