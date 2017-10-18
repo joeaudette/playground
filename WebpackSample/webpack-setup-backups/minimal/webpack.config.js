3c8dc58b81a8fedf4df96c487c58cf9a43431480
Joe Audette <joe.audette@gmail.com>
2017-10-18 17:41:22 -0400

    backup current webpack

---
+++ WebpackSample/webpack-setup-backups/minimal/webpack.config.js
@@ -0,0 +1,58 @@
+﻿const path = require('path');
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const config = {
+    entry: {
+        'clientapp1': ['./ClientApp1/Main.ts'],
+        'mainstyle': './scss/style.scss'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'wwwroot/dist/'),
+        publicPath: 'dist/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                }),
+                exclude: /node_modules/
+            },
+            { 
+                test: /\.(sass|scss)$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                }),
+                
+                
+            },
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ]
+    },
+    resolve: {
+        modules: [path.join(__dirname,"./node_modules/")],
+        //extensions: [".tsx", ".ts", ".js", ".scss"]
+    },
+    devtool: 'source-map',
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
+        new ExtractTextPlugin({
+            filename: '[name].bundle.css',
+            allChunks: true,
+        }),
+       
+       
+    ]
+};
+
+module.exports = config;
