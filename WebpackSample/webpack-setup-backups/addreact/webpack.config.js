5ab1ac14748e66e746f29cd1f65396698a563fbe
Joe Audette <joe.audette@gmail.com>
2017-10-19 08:13:15 -0400

    make backup of setup with react

---
+++ WebpackSample/webpack-setup-backups/addreact/webpack.config.js
@@ -0,0 +1,67 @@
+﻿const path = require('path');
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+//const CheckerPlugin = require('awesome-typescript-loader').CheckerPlugin;
+
+
+const config = {
+    entry: {
+        'app-react': './app-react/boot.tsx',
+        'clientapp1': ['./ClientApp1/Main.ts'],
+        'mainstyle': './scss/style.scss'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'wwwroot/dist/'),
+        publicPath: 'dist/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                }),
+                exclude: /node_modules/
+            },
+            { 
+                test: /\.(sass|scss)$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                }),
+                
+                
+            },
+            //{
+            //    test: /\.tsx?$/,
+            //    include: /ClientApp/,
+            //    use: 'awesome-typescript-loader?silent=true'
+            //},
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ]
+    },
+    resolve: {
+        modules: [path.join(__dirname,"./node_modules/")],
+        extensions: [".tsx", ".ts", ".js", ".scss"]
+    },
+    devtool: 'source-map',
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
+        new ExtractTextPlugin({
+            filename: '[name].bundle.css',
+            allChunks: true,
+        }),
+       
+       
+    ]
+};
+
+module.exports = config;
